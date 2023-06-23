34feac1b025466a4579ae41df90994a3544c88c1
Sourcery AI <>
2023-06-23 09:56:01 +0000

    'Refactored by Sourcery'

--- files/3rd-party-licenses.py
+++ files/3rd-party-licenses.py
@@ -9,31 +9,29 @@ license_json = 'vernemq-dependency-licenses.json'
 
 with open(license_json, 'r') as f:
     licenses = json.load(f)
-f = open("3rd-party-licenses.txt","w")
-f.write("3rd-party Licenses Summary\n")
-f.write("==========================\n")
-for l in licenses['dependencies']:
-    license_ids = ""
-    has_other_licenses = False
-    for ll in l['licenses']:
-        if ll['id'] == None:
-            has_other_licenses = True
-        else:
-            license_ids += " " + ll['id']
-    if has_other_licenses:
-        license_ids += " (others see below)"
-    f.write("%s: %s\n" % (l['name'], license_ids))
+with open("3rd-party-licenses.txt","w") as f:
+    f.write("3rd-party Licenses Summary\n")
+    f.write("==========================\n")
+    for l in licenses['dependencies']:
+        license_ids = ""
+        has_other_licenses = False
+        for ll in l['licenses']:
+            if ll['id'] is None:
+                has_other_licenses = True
+            else:
+                license_ids += " " + ll['id']
+        if has_other_licenses:
+            license_ids += " (others see below)"
+        f.write("%s: %s\n" % (l['name'], license_ids))
 
-f.write("\n3rd-party Licenses Detailed\n")
-f.write("===========================\n")
-for l in licenses['dependencies']:
-    f.write("\n%s:\n" % l['name'])
-    f.write("---------------------------------------\n")
-    for ll in l['licenses']:
-        if ll['content']:
-            f.write(ll['content'].encode('utf8'))
-            f.write("\n")
-
-f.close()
+    f.write("\n3rd-party Licenses Detailed\n")
+    f.write("===========================\n")
+    for l in licenses['dependencies']:
+        f.write("\n%s:\n" % l['name'])
+        f.write("---------------------------------------\n")
+        for ll in l['licenses']:
+            if ll['content']:
+                f.write(ll['content'].encode('utf8'))
+                f.write("\n")
 
 os.remove(license_json)
